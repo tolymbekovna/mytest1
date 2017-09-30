78c7311fb31c05b6826b0e53d7f6a0beab576b85
Tolganay Tolymmbek <tolymbekovna@gmail.com>
2017-10-01 01:34:47 +0600

    ushunshi commit

---
+++ src/kz/kaznitu/lessons/MyMath.java
@@ -0,0 +1,10 @@
+package kz.kaznitu.lessons;
+
+public class MyMath {
+    public static int kvadrat (int a){
+        return a*a;
+    }
+    public static int kub(int a){
+        return a*a*a;
+    }
+}
