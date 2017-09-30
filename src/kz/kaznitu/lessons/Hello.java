82009ae75e1b3c83ef6b1658a64e9b620d35ed91
Tolganay Tolymmbek <tolymbekovna@gmail.com>
2017-10-01 01:03:13 +0600

    ozgeris commity

--- src/kz/kaznitu/lessons/Hello.java
+++ src/kz/kaznitu/lessons/Hello.java
@@ -3,5 +3,7 @@ package kz.kaznitu.lessons;
 public class Hello {
     public static void main (String[]args){
         System.out.println("Hello world");
+        System.out.println("Bul jumis maksaty");
+        System.out.println("github-pen jumis jasap uirenu");
     }
 }
