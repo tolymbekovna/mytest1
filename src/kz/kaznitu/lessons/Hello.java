fa8ec610ecda8d05c42a37a4629d45d6d2161024
Tolganay Tolymmbek <tolymbekovna@gmail.com>
2017-10-01 01:48:39 +0600

    4shi jana butaktagy commit

--- src/kz/kaznitu/lessons/Hello.java
+++ src/kz/kaznitu/lessons/Hello.java
@@ -5,5 +5,11 @@ public class Hello {
         System.out.println("Hello world");
         System.out.println("Bul jumis maksaty");
         System.out.println("github-pen jumis jasap uirenu");
+
+   int a = MyMath.kvadrat(5);
+    int b = MyMath.kub(5);
+    System.out.println("a="+a);
+    System.out.println("b="+b);
+    System.out.println(MyMath.max(8,9));
     }
 }
--- src/kz/kaznitu/lessons/MyMath.java
+++ src/kz/kaznitu/lessons/MyMath.java
@@ -7,4 +7,14 @@ public class MyMath {
     public static int kub(int a){
         return a*a*a;
     }
+    public static boolean isEven(int a){
+        return (a%2==0);
+    }
+    public static int max(int a, int b){
+        if (a<=b)
+            return b;
+        else
+            return a;
+
+    }
 }
