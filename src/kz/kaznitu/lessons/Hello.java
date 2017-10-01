7aefe7c0033d992c97d4ad81320c8c8f297219bc
Tolganay Tolymmbek <tolymbekovna@gmail.com>
2017-10-01 09:40:54 +0600

    masterdegi ozgeris

--- src/kz/kaznitu/lessons/Hello.java
+++ src/kz/kaznitu/lessons/Hello.java
@@ -1,9 +1,17 @@
 package kz.kaznitu.lessons;
 
 public class Hello {
-    public static void main (String[]args){
+    public static void main (String[]args) {
         System.out.println("Hello world");
         System.out.println("Bul jumis maksaty");
         System.out.println("github-pen jumis jasap uirenu");
+
+        int a = MyMath.kvadrat(5);
+        int b = MyMath.kub(5);
+        System.out.println("a=" + a);
+        System.out.println("b=" + b);
+        System.out.println(MyMath.max3(8, 9, 12));
+        System.out.println(MyMath.isEven(12));
+        System.out.println(MyMath.isEven(7));
     }
 }
